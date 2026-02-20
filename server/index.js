2a954774ec454a8c27d9699e0e7c2096179e1e83
Aadhil <aadhildev2025@gmail.com>
2026-02-20 21:10:01 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -25,6 +25,10 @@ app.use('/api/products', require('./routes/productRoutes'));
 app.use('/api/store', require('./routes/storeRoutes'));
 app.use('/api/sales', require('./routes/saleRoutes'));
 
+app.get('/api/health', (req, res) => {
+    res.json({ status: 'ok', message: 'Backend is reachable' });
+});
+
 app.get('/', (req, res) => {
     res.send('POS API is running...');
 });
